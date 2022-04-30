007fc90edbd674b50c5edf88807152ee6e353d7c
aldin <a21aldsa@student.his.se>
2022-04-30 10:09:07 +0200

    created button in activity_main

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -2,18 +2,7 @@ package com.example.screens;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.screens.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
