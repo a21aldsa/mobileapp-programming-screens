b12ca04da9bd4d3814de970638d1d688bb7a7225
aldin <a21aldsa@student.his.se>
2022-05-03 11:53:54 +0200

    created intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("name", "Halloj");
+        intent.putExtra("number", 50);
+        startActivity(intent);
+
     }
 }
