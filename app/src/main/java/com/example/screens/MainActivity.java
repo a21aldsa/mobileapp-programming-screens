a466007bcc718957689527c2df20738d7e2ba83f
aldin <a21aldsa@student.his.se>
2022-05-03 12:20:40 +0200

    set button to start second activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.screens;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra("name", "Halloj");
-        intent.putExtra("number", 50);
-        startActivity(intent);
-
+        Button button = findViewById(R.id.mainButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Halloj");
+                intent.putExtra("number", 50);
+                startActivity(intent);
+            }
+        });
     }
 }
